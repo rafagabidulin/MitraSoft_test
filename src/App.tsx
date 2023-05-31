ff61962a8e16d3248cf233cc9db4a350ca32e9c8
rafagabidulin <rafagabidulin@gmail.com>
2023-05-31 21:55:24 +0300

    created Layout component, fixed App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,22 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+import Layout from './components/Layout/Layout';
 import PostsPage from './pages/PostsPage/PostsPage';
+import AboutMePage from './pages/AboutMePage/AboutMePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
 export const App = () => (
   <div>
     <BrowserRouter>
       <Provider store={store}>
-        <Routes>
-          <Route path='/' element={<PostsPage />} />
-        </Routes>
+        <Layout>
+          <Routes>
+            <Route path='/' element={<PostsPage />} />
+            <Route path='/about' element={<AboutMePage />} />
+            <Route path='*' element={<NotFoundPage />} />
+          </Routes>
+        </Layout>
       </Provider>
     </BrowserRouter>
   </div>

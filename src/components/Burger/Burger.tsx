c844e0ccaaefaa5bae177948c8ec54226424f704
rafagabidulin <rafagabidulin@gmail.com>
2023-06-02 01:21:49 +0300

    fixed burger menu

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import { NavLink } from 'react-router-dom';
 
 import user from '../../../public/images/user.png';
@@ -11,18 +13,26 @@ const Burger = ({ show, handleClose }) => (
       <Offcanvas.Title>Menu</Offcanvas.Title>
     </Offcanvas.Header>
     <Offcanvas.Body>
-      <div>
-        <NavLink to='/about'>
-          <img className='my-2' src={user} alt='user' width='50px' />
-        </NavLink>
-        <p>Rafael Gabidulin</p>
-        <a href='mailto: rafagabidulin@gmail.com'>rafagabidulin@gmail.com</a>
-      </div>
+      <Card className='border m-0'>
+        <Card.Body>
+          <NavLink to='/about'>
+            <img className='my-2' src={user} alt='user' width='50px' />
+          </NavLink>
+          <h2>Rafael Gabidulin</h2>
+          <b>
+            Email: <a href='mailto: rafagabidulin@gmail.com'>rafagabidulin@gmail.com</a>
+          </b>
+        </Card.Body>
+      </Card>
       <Nav.Item>
-        <NavLink to='about'>About me</NavLink>
+        <NavLink to='about'>
+          <Button className='my-2 w-100'>About Me</Button>
+        </NavLink>
       </Nav.Item>
       <Nav.Item>
-        <NavLink to='/'>Posts</NavLink>
+        <NavLink to='/'>
+          <Button className='my-2 w-100'>Posts</Button>
+        </NavLink>
       </Nav.Item>
     </Offcanvas.Body>
   </Offcanvas>
